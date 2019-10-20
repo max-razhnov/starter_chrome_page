dd92acb1d1f297281a46ac492808a25fde2afbb6
max razhnov <shahteryshka@gmail.com>
2019-10-20 23:18:03 +0300

    chore: initial commit

---
+++ src/js/app.js
@@ -0,0 +1,71 @@
+const time = document.getElementsByClassName("time")[0],
+  datePlace = document.getElementsByClassName("date")[0],
+  author = document.getElementsByClassName("title_author")[0],
+  title = document.getElementsByClassName("title_name")[0],
+  background = document.querySelector("body");
+function setTime() {
+  const date = new Date(),
+    hours = 12 - 24 + date.getHours(),
+    AMPM = date.getHours() >= 12 ? "PM" : "AM";
+  time.innerText = `${hours}:${addZero(date.getMinutes())}:${addZero(
+    date.getSeconds()
+  )} ${AMPM}`;
+  setTimeout(setTime, 1000);
+}
+function addZero(count) {
+  return count > 9 ? count : `0` + count;
+}
+function setDate() {
+  const date = new Date();
+  datePlace.innerText = `${date.getDate()}.${date.getMonth() +
+    1}.${date.getFullYear()}`;
+}
+function setTitle() {
+  const date = new Date(),
+    hours = date.getHours();
+  if (hours < 10) {
+    background.style.color = "#000";
+    background.style.background = `url('./roland-hechanova-f372RxIk18U-unsplash.jpg') no-repeat center/ cover`;
+    title.innerText = "Good morning!";
+  } else if (hours < 15) {
+    background.style.background = `url('./arnaud-mariat-VZvWjqh-wJU-unsplash.jpg') no-repeat center/ cover`;
+    title.innerText = "Good afternoon!";
+  } else if (hours < 22) {
+    background.style.background = `url('./lerone-pieters-XVaXbzQul90-unsplash.jpg') no-repeat center/ cover`;
+    title.innerText = "Good evening!";
+  } else {
+    background.style.color = "rgba(255,255,255,1)";
+    background.style.background = `url('./blake-carpenter-oJbglDa4cO0-unsplash.jpg') no-repeat center/ cover`;
+    title.innerText = "Good night!";
+  }
+}
+author.addEventListener("keypress", setName);
+author.addEventListener("blur", setName);
+function setName(ev) {
+  if (ev.type === "keypress") {
+    if (ev.keyCode === 13) {
+      localStorage.setItem("name", ev.currentTarget.innerText);
+      author.blur();
+    }
+  } else {
+    if (ev.currentTarget.innerText.length > 3) {
+      localStorage.setItem("name", ev.currentTarget.innerText);
+    } else {
+      author.textContent = "[Enter your Name]";
+    }
+  }
+}
+function getName() {
+  if (
+    localStorage.getItem("name") === null ||
+    localStorage.getItem("name") === ""
+  ) {
+    author.textContent = "[Enter your Name]";
+  } else {
+    author.textContent = localStorage.getItem("name");
+  }
+}
+setTime();
+setDate();
+setTitle();
+getName();
